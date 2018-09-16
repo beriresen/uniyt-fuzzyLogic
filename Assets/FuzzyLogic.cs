b920a048d7d257ea79d07c909abc7c94ea0968c1
beriresen <senberire@gmail.com>
2018-09-16 13:48:52 +0300

    fuzzy

---
+++ Assets/FuzzyLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuzzyLogic : MonoBehaviour {
+
+    public InputField healthInput;
+    public Text healthyText;
+    public Text hurtText;
+    public Text criticalText;
+
+    float healthVal, criticalVal, hurtVal;
+
+    public AnimationCurve healthy;
+    public AnimationCurve hurt;
+    public AnimationCurve critical;
+
+
+    private void Start()
+    {
+        SetTexts();
+    }
+
+    public void EvaluateParameter()
+    {
+        if (string.IsNullOrEmpty(healthInput.text)) return;
+
+        float health = float.Parse(healthInput.text);
+
+        healthVal = healthy.Evaluate(health);
+        hurtVal = hurt.Evaluate(health);
+        criticalVal = critical.Evaluate(health);
+
+        SetTexts();
+    }
+
+    private void SetTexts()
+    {
+        healthyText.text = healthVal.ToString();
+        criticalText.text = criticalVal.ToString();
+        hurtText.text = hurtVal.ToString();
+    }
+}
